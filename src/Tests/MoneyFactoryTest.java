d5cb276157e0ef440b90179f43695d8477c75cfd
bankkeez <sethitikul@gmail.com>
2018-02-16 17:21:44 +0700

    add method to test

--- src/Tests/MoneyFactoryTest.java
+++ src/Tests/MoneyFactoryTest.java
@@ -3,19 +3,25 @@ package Tests;
 import static org.junit.Assert.*;
 
 import org.junit.Test;
-
+import Factory.MalayMoneyFactory;
 import Factory.MoneyFactory;
 import Factory.ThaiMoneyFactory;
 
 public class MoneyFactoryTest {
 	@Test
-	public void testSetAndGetInstance() {
+	public void testSetAndGetInstanceThai() {
 		MoneyFactory.setFactory(new ThaiMoneyFactory());
 		assertEquals(MoneyFactory.getInstance().getClass().getSimpleName(), "ThaiMoneyFactory");
 	}
 
 	@Test
+	public void testSetAndGetInstanceMalay() {
+		MoneyFactory.setFactory(new MalayMoneyFactory());
+		assertEquals(MoneyFactory.getInstance().getClass().getSimpleName(), "MalayMoneyFactory");
+	}
+
+	@Test
 	public void testBankNoteSeialNumber() {
-		
+
 	}
 }
