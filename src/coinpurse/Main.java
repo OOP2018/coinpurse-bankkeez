0c1a83c068d40de5b09943dd130bcf75e533e329
bankkeez <sethitikul@gmail.com>
2018-02-23 13:51:22 +0700

    updated main

--- src/coinpurse/Main.java
+++ src/coinpurse/Main.java
@@ -4,23 +4,27 @@ import Utilities.ConsoleDialog;
 import Utilities.Purse;
 
 /**
- * A main class to create objects and connect objects together.
- * The user interface needs a reference to coin purse.
+ * A main class to create objects and connect objects together. The user
+ * interface needs a reference to coin purse.
+ * 
  * @author your name
  */
 public class Main {
 
-    /**
-     * Configure and start the application.
-     * @param args not used
-     */
-    public static void main( String[] args ) {
-        // 1. create a Purse
-    	Purse purse = new Purse(10);
-        // 2. create a ConsoleDialog with a reference to the Purse object
-    	ConsoleDialog ui = new ConsoleDialog(purse);
-        // 3. run the ConsoleDialog
-    	ui.run();
-
-    }
+	/**
+	 * Configure and start the application.
+	 * 
+	 * @param args
+	 *            not used
+	 */
+	public static void main(String[] args) {
+		// reads the file
+		ReadFile.read();
+		// 1. create a Purse
+		Purse purse = new Purse(10);
+		// 2. create a ConsoleDialog with a reference to the Purse object
+		ConsoleDialog ui = new ConsoleDialog(purse);
+		// 3. run the ConsoleDialog
+		ui.run();
+	}
 }
